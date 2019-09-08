61cc1abcb11e9de68b72e37477de9996ae3f784f
hama-matcha <hamamatcha@gmail.com>
2019-09-08 14:32:11 +0900

    試走会2用ゲーム

--- RunMethods/Status/CourseRun.cpp
+++ RunMethods/Status/CourseRun.cpp
@@ -23,9 +23,9 @@ void CourseRun::init(){
 
 void CourseRun::setNextState(){
     if(this->course == L) {
-        nextState = STOP;
+        nextState = PARKING;
     }else if(this->course == R){
-        nextState = STOP;
+        nextState = PARKING;
     }
 }
 
@@ -58,33 +58,35 @@ void CourseRun::createCourseL(){
     patterns.push_back(new RunPattern(LINE_TRACE, 55, DISTANCE, 30, 0.25, 0.01, 0.03, 40));
 
     patterns.push_back(new RunPattern(LINE_TRACE, 35, DISTANCE, 20, 0.7, 0.01, 0.03, 40));
-    patterns.push_back(new RunPattern(LINE_TRACE, 20, COLOR, 4, 0.3, 0.01, 0.03, 40));
+    // patterns.push_back(new RunPattern(LINE_TRACE, 20, COLOR, 4, 0.3, 0.01, 0.03, 40));
 }
 
 void CourseRun::createCourseR(){
-    patterns.push_back(new RunPattern(LINE_TRACE, 30, DISTANCE, 20, 0.7, 0.0, 0.03, 40, RIGHT));
-    patterns.push_back(new RunPattern(LINE_TRACE, 50, DISTANCE, 50, 0.25, 0.0, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 50, DISTANCE, 20, 0.7, 0.01, 0.03, 40, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 70, DISTANCE, 50, 0.25, 0.01, 0.03, 60, RIGHT));
 
-    patterns.push_back(new RunPattern(LINE_TRACE, 50, DISTANCE, 70, 0.35, 0.0, 0.03, 60, RIGHT));
-    patterns.push_back(new RunPattern(LINE_TRACE, 50, DISTANCE, 70, 0.25, 0.0, 0.03, 60, RIGHT));
-    patterns.push_back(new RunPattern(LINE_TRACE, 50, DISTANCE, 70, 0.35, 0.0, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 70, DISTANCE, 70, 0.35, 0.01, 0.04, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 70, DISTANCE, 70, 0.25, 0.01, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 70, DISTANCE, 70, 0.35, 0.01, 0.04, 60, RIGHT));
 
     // s字
-    patterns.push_back(new RunPattern(LINE_TRACE, 40, DISTANCE, 70, 0.5, 0.01, 0.03, 60, RIGHT));
-    patterns.push_back(new RunPattern(LINE_TRACE, 30, DISTANCE, 30, 0.6, 0.01, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 55, DISTANCE, 70, 0.5, 0.01, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 55, DISTANCE, 30, 0.5, 0.01, 0.03, 60, RIGHT));
 
     // GATE1からの円
-    patterns.push_back(new RunPattern(LINE_TRACE, 30, DISTANCE, 100, 0.65, 0.01, 0.05, 60, RIGHT));
-    patterns.push_back(new RunPattern(LINE_TRACE, 45, DISTANCE, 20, 0.25, 0.01, 0.03, 60, RIGHT)); // 500
+    patterns.push_back(new RunPattern(LINE_TRACE, 45, DISTANCE, 120, 0.65, 0.03, 0.04, 60, RIGHT)); // 適切な値 ふらつく
+    patterns.push_back(new RunPattern(LINE_TRACE, 55, DISTANCE, 20, 0.4, 0.01, 0.03, 60, RIGHT)); // 500
 
-    patterns.push_back(new RunPattern(LINE_TRACE, 40, DISTANCE, 90, 0.45, 0.01, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 55, DISTANCE, 90, 0.5, 0.03, 0.04, 60, RIGHT));
 
-    patterns.push_back(new RunPattern(LINE_TRACE, 50, DISTANCE, 90, 0.25, 0.01, 0.03, 60, RIGHT));
-    patterns.push_back(new RunPattern(LINE_TRACE, 35, DISTANCE, 70, 0.5, 0.01, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 55, DISTANCE, 90, 0.25, 0.01, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 55, DISTANCE, 70, 0.5, 0.01, 0.04, 60, RIGHT)); // GATE2
 
-    patterns.push_back(new RunPattern(LINE_TRACE, 40, DISTANCE, 10, 0.25, 0.01, 0.03, 60, RIGHT));
-    patterns.push_back(new RunPattern(LINE_TRACE, 30, DISTANCE, 30, 0.55, 0.01, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 55, DISTANCE, 10, 0.25, 0.01, 0.03, 60, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 45, DISTANCE, 30, 0.5, 0.03, 0.04, 60, RIGHT));
 
-    patterns.push_back(new RunPattern(LINE_TRACE, 30, DISTANCE, 150, 0.65, 0.01, 0.05, 60, RIGHT));
-    patterns.push_back(new RunPattern(LINE_TRACE, 50, DISTANCE, 60, 0.25, 0.01, 0.03, 80, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 45, DISTANCE, 150, 0.5, 0.02, 0.03, 60, RIGHT)); // 最後の円
+    patterns.push_back(new RunPattern(LINE_TRACE, 55, DISTANCE, 30, 0.25, 0.01, 0.03, 40, RIGHT));
+
+    patterns.push_back(new RunPattern(LINE_TRACE, 35, DISTANCE, 20, 0.7, 0.01, 0.03, 40, RIGHT));
 }
--- RunMethods/Status/Parking.cpp
+++ RunMethods/Status/Parking.cpp
@@ -23,10 +23,30 @@ void Parking::setNextState(){
 }
 
 void Parking::createCourseL(){
-    patterns.push_back(new RunPattern(LINE_TRACE, 15, COLOR, 2, 1.3, 0.0, 0.02, 40, LEFT)); 
+    //直線
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, COLOR, 2, 1.3, 0.0, 0.02, 40));
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, COLOR, 1, 1.3, 0.0, 0.02, 40));
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, COLOR, 2, 1.3, 0.0, 0.02, 40));
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, COLOR, 1, 1.3, 0.0, 0.02, 40));
+
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, DISTANCE, 60, 1.3, 0.0, 0.02, 40));
+    patterns.push_back(new RunPattern(LINE_TRACE, 20, COLOR, 4, 1.3, 0.0, 0.02, 40));
+
+    patterns.push_back(new RunPattern(TURNING, 30, DIRECTION, 65, DIRECTION_LEFT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 20, DISTANCE, 12, 1.3, 0.0, 0.02, 40));
+    patterns.push_back(new RunPattern(SPIN, 30, DIRECTION, 70, DIRECTION_RIGHT));
+    patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1));
+    patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1));
+    patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1));
+    patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1));
+    patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1));
+    patterns.push_back(new RunPattern(SPIN, 20, DIRECTION, 10, DIRECTION_LEFT));
+
+
+    // patterns.push_back(new RunPattern(LINE_TRACE, 15, COLOR, 2, 1.3, 0.0, 0.02, 40)); 
     patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1)); // ここまでの2行はゲーム攻略の方に含めるほうが良きかな
 
-    patterns.push_back(new RunPattern(SPIN, 30, DIRECTION, 70, DIRECTION_RIGHT));
+    patterns.push_back(new RunPattern(SPIN, 30, DIRECTION, 75, DIRECTION_RIGHT));
     patterns.push_back(new RunPattern(LINE_TRACE, 15, COLOR, 2, 1.3, 0.0, 0.02, 40, RIGHT));
     patterns.push_back(new RunPattern(LINE_TRACE, 15, DISTANCE, 10, 1.3, 0.0, 0.02, 40, RIGHT));
 
@@ -36,6 +56,25 @@ void Parking::createCourseL(){
 }
 
 void Parking::createCourseR(){
+    //直線
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, COLOR, 2, 1.3, 0.0, 0.02, 40, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, COLOR, 1, 1.3, 0.0, 0.02, 40, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, COLOR, 2, 1.3, 0.0, 0.02, 40, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, COLOR, 1, 1.3, 0.0, 0.02, 40, RIGHT));
+    //曲がる
+    patterns.push_back(new RunPattern(LINE_TRACE, 30, DISTANCE, 60, 1.3, 0.0, 0.02, 40, RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 20, COLOR, 4, 1.3, 0.0, 0.02, 40, RIGHT));
+
+    patterns.push_back(new RunPattern(TURNING, 30, DIRECTION, 65, DIRECTION_RIGHT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 20, COLOR, 5, 12, 1.3, 0.0, 0.02, 40));
+    patterns.push_back(new RunPattern(TURNING, 30, DIRECTION, 65, DIRECTION_LEFT));
+    patterns.push_back(new RunPattern(LINE_TRACE, 20, COLOR, 5, 12, 1.3, 0.0, 0.02, 40));
+    patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1));
+    patterns.push_back(new RunPattern(LINE_TRACE, 20, COLOR, 2, 12, 1.3, 0.0, 0.02, 40));
+    patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1));
+
+
+
     patterns.push_back(new RunPattern(LINE_TRACE, 15, COLOR, 2, 1.3, 0.0, 0.02, 40, LEFT)); 
     patterns.push_back(new RunPattern(STRAIGHT, 20, DISTANCE, 25));
     patterns.push_back(new RunPattern(SPIN, 30, DIRECTION, 65, DIRECTION_LEFT));
