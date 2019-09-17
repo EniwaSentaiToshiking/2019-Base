a354f9da8c7d0e89c6048110c84cb99722dc3bc0
vivid344 <vivid344@icloud.com>
2019-09-17 16:53:18 +0900

    fix: 細かい単位の動きを調整

--- RunMethods/Status/Game.cpp
+++ RunMethods/Status/Game.cpp
@@ -30,30 +30,30 @@ void Game::straight(){ // 色を検知するまで直進
     } else {
         edge = RIGHT;
     }
-    patterns.push_back(new RunPattern(LINE_TRACE, 20, DISTANCE, 7, 1.5, 0.0, 0.1, 25, edge));
-    patterns.push_back(new RunPattern(LINE_TRACE, 20, COLOR, 8, 0.7, 0.0, 0.03, 25, edge));
+    patterns.push_back(new RunPattern(LINE_TRACE, 20, DISTANCE, 5, 0.5, 0.0, 0.1, 25, edge));
+    patterns.push_back(new RunPattern(LINE_TRACE, 20, COLOR, 8, 0.3, 0.0, 0.1, 25, edge));
 }
 
 void Game::jumpCircle(){ // サークルを直進で乗り超える
-    Edge edge;
-    if (edge_line == Line_L) {
-        edge = LEFT;
+    patterns.push_back(new RunPattern(STRAIGHT, 20, DISTANCE, 12));
+
+    if (edge_line == Line_R) {
+        patterns.push_back(new RunPattern(TURNING, 20, DIRECTION, 20, DIRECTION_RIGHT));
     } else {
-        edge = RIGHT;
+        patterns.push_back(new RunPattern(TURNING, 20, DIRECTION, 20, DIRECTION_LEFT));
     }
-    patterns.push_back(new RunPattern(STRAIGHT, 20, DISTANCE, 15));
-    patterns.push_back(new RunPattern(LINE_TRACE, 20, DISTANCE, 5, 0.7, 0.0, 0.03, 25, edge));
+    straight();
 }
 
 void Game::turnR(){ // 右90度旋回
     if (edge_line == Line_R) {
-        patterns.push_back(new RunPattern(TURNING, 20, DIRECTION, 105, DIRECTION_RIGHT));
+        patterns.push_back(new RunPattern(TURNING, 20, DIRECTION, 107, DIRECTION_RIGHT));
         edge_line = Line_R;
     } else {
         patterns.push_back(new RunPattern(TURNING, 20, DIRECTION, 95, DIRECTION_RIGHT));
         edge_line = Line_R;
     }
-    
+    straight();
 }
 
 void Game::turnL(){ // 左90度旋回
@@ -61,53 +61,49 @@ void Game::turnL(){ // 左90度旋回
         patterns.push_back(new RunPattern(TURNING, 20, DIRECTION, 95, DIRECTION_LEFT));
         edge_line = Line_L;
     } else {
-        patterns.push_back(new RunPattern(TURNING, 20, DIRECTION, 105, DIRECTION_LEFT));
+        patterns.push_back(new RunPattern(TURNING, 20, DIRECTION, 107, DIRECTION_LEFT));
         edge_line = Line_L;
     }
+    straight();
 }
 
 void Game::turnBack(){ // 180度旋回
     if (edge_line == Line_R) {
-        patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, 170, DIRECTION_LEFT));
-        patterns.push_back(new RunPattern(STRAIGHT, 10, DISTANCE, 1));
-        patterns.push_back(new RunPattern(BRAKE, 0, CLOCK, 300));
-        patterns.push_back(new RunPattern(LINE_TRACE, 10, DISTANCE, 7, 0.7, 0.0, 0.03, 25, RIGHT));
+        patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, 150, DIRECTION_LEFT));
+        
     } else {
-        patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, 170, DIRECTION_RIGHT));
-        patterns.push_back(new RunPattern(STRAIGHT, 10, DISTANCE, 1));
-        patterns.push_back(new RunPattern(BRAKE, 0, CLOCK, 300));
-        patterns.push_back(new RunPattern(LINE_TRACE, 10, DISTANCE, 7, 0.7, 0.0, 0.03, 25, LEFT));
+        patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, 150, DIRECTION_RIGHT));
+
     }
+    straight();
 }
 
 void Game::release(int direction){ // ブロックのリリース
     if (direction < 180) {
-        patterns.push_back(new RunPattern(STRAIGHT, 20, DISTANCE, 10));
-        patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, direction, DIRECTION_RIGHT));
+        patterns.push_back(new RunPattern(STRAIGHT, 15, DISTANCE, 10));
+        patterns.push_back(new RunPattern(SPIN, 15, DIRECTION, direction, DIRECTION_RIGHT));
         patterns.push_back(new RunPattern(ARM, 180, 100));
-        patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, direction, DIRECTION_LEFT));
-        patterns.push_back(new RunPattern(STRAIGHT, -20, DISTANCE, 5));
+        patterns.push_back(new RunPattern(SPIN, 15, DIRECTION, direction + 10, DIRECTION_LEFT));
+        patterns.push_back(new RunPattern(STRAIGHT, -15, DISTANCE, 8));
     } else {
-        patterns.push_back(new RunPattern(STRAIGHT, 20, DISTANCE, 10));
-        patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, 360 - direction, DIRECTION_LEFT));
+        patterns.push_back(new RunPattern(STRAIGHT, 15, DISTANCE, 10));
+        patterns.push_back(new RunPattern(SPIN, 15, DIRECTION, 360 - direction, DIRECTION_LEFT));
         patterns.push_back(new RunPattern(ARM, 180, 100));
-        patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, 360 - direction, DIRECTION_RIGHT));
-        patterns.push_back(new RunPattern(STRAIGHT, -20, DISTANCE, 5));
+        patterns.push_back(new RunPattern(SPIN, 15, DIRECTION, 360 - direction + 8, DIRECTION_RIGHT));
+        patterns.push_back(new RunPattern(STRAIGHT, -15, DISTANCE, 8));
     }
 }
 
 void Game::parking(int direction){ // 駐車に向けた動き 
     if (direction == 0) {
-        patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1)); 
     } else if (direction < 180) {
         patterns.push_back(new RunPattern(STRAIGHT, 20, DISTANCE, 5));
         patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, direction, DIRECTION_RIGHT));
-        patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1)); 
     } else {
         patterns.push_back(new RunPattern(STRAIGHT, 20, DISTANCE, 5));
         patterns.push_back(new RunPattern(SPIN, 10, DIRECTION, 360 - direction, DIRECTION_LEFT));
-        patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1)); 
     }
+    patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1)); 
 }
 
 void Game::createCourseL(){
@@ -116,13 +112,24 @@ void Game::createCourseL(){
     int bonus[2]={block.bonus_pos, block.bonus_color};
     int number = block.number;
 
-    straight();
-    jumpCircle();
-    straight();
-    release(35);
-    turnBack();
-    straight();
-    // patterns.push_back(new RunPattern(STRAIGHT, 20, BLACKLINE, 1)); 
+    turnR();
+
+    for (int i = 0; i < 10; i++){
+        turnL();
+        turnL();
+        release(35);
+        turnR();
+        turnL();
+        release(325);
+        jumpCircle();
+        release(35);
+        turnL();
+        turnL();
+        turnR();
+        release(325);
+        turnL();
+        jumpCircle();
+    }
 }
 
 void Game::createCourseR(){
